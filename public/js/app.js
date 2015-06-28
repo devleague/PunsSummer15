e4c9d726e5179eef893fc07807fa5acab7d4f6d4
Jay Won <jason@sudokrew.com>
2015-06-27 22:17:36 -1000

    Moved views to server and fixed app.js syntax errors.

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,10 @@
 $(function(){
 
-// This will target the submit_button ID & attach a click event, which runs a function.
+  // This will target the submit_button ID & attach a click event, which runs a function.
   $("#submit_button").click(function(){ 
-
-//  This function will first create a variable (Storing in memory) with the Value (.val) of the targeted element which in this case is the ID user_input
+    //  This function will first create a variable (Storing in memory) with the Value (.val) of the targeted element which in this case is the ID user_input
     var user_in = $("#user_input").val();
+  });
 
 
   function addUserInput(userInput){
@@ -50,11 +50,11 @@ $(function(){
     // This will create a variable named last_li setting the value to the last created li element in the #main_list element
     var last_li = $("#main_list").find('li:last');
 
-   // notice when targeting a created variable we don't enter it as a string.
+    // notice when targeting a created variable we don't enter it as a string.
     $(last_li).append('<input class="delete_button" type="button" value="delete">');
     $(last_li).prepend('<input class="checkbox" type="checkbox">');
 
-// This block of code puts a click event on each 
+    // This block of code puts a click event on each 
     $(".checkbox").click(function(){
       var parentLi = $(".checkbox").closest("li");
       var completed;
@@ -65,37 +65,38 @@ $(function(){
         // $(this).parent().css("text-decoration", "line-through");     
         $(this).parent().css("text-decoration", "line-through");
       
-      } else {
+      } 
+      else {
         completed = false;
         // $(this).parent().css("text-decoration","none");
             parentLi.css("text-decoration", "none");
       }
 
+      $('#save_button').click(function(){
+        // text that user inputs
+        console.log(user_in);
 
+
+        var send_data = {data : user_in};
+        $.post("/todoitam", send_data,function (data){
+          $(".result").html(data);
+        });      
+      });
+
+      // We target classes with a . before the name and ids with a # before the name so .delete_button and #main_list!
+      $(".delete_button").click(function(){
+        $.ajax('/todoitam/:item_id',
+          { type:"DELETE",
+            success :function(data){
+              console.log('data',data);
+            }
+          });
+          $(this).parent().remove();
+
+        //  This will target the user_input ID and clear the field by setting the value to an empty string.
+        // our code runs in order from top to bottom (in most cases) so the form here won't clear until after the lines above it
+        $("#user_input").val("");
+      });
     });
-    $('#save_button').click(function(){
-      // text that user inputs
-      console.log(user_in);
-
-
-      var send_data = {data : user_in};
-      $.post("/todoitam", send_data,function (data){
-        $(".result").html(data);
-      });      
-    });
-
-    // We target classes with a . before the name and ids with a # before the name so .delete_button and #main_list!
-    $(".delete_button").click(function(){
-      $.ajax('/todoitam/:item_id',
-        { type:"DELETE",
-          success :function(data){
-            console.log('data',data);
-          }
-        });
-        $(this).parent().remove();
-    });
-//  This will target the user_input ID and clear the field by setting the value to an empty string.
-    // our code runs in order from top to bottom (in most cases) so the form here won't clear until after the lines above it
-    $("#user_input").val("");
-  });
+  }
 });
